48888021d8efc477b3fa0eca6ea3f9e8e47b6023
Daryl <daryl.9@gmail.com>
2022-02-01 16:15:16 +0800

    show 144

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show 
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def edit 
         @user = User.find(params[:id])
     end
