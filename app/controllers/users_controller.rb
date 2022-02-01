47586fcb60ea3b2ee4aa7b55e667d20da1fd46db
Daryl <daryl.9@gmail.com>
2022-02-01 09:49:23 +0800

    144

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
     def create 
         puts "Creating users"
         @user = User.new(user_params)
